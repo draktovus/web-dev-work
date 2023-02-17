713acec235d39e0d36bc75a766686c352ec75518
Draktovus <drakanez@gmail.com>
2023-02-16 20:49:28 -0500

    Formatted whitespace
    - index.js
    - login.pug

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 //create nodejs express server
 const http = require('http')
 const express = require('express');
-const app =  express();
+const app = express();
 
 // 127.0.0.1 is the loopback address
 const hostname = '127.0.0.1';
@@ -10,13 +10,13 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index', {title:"ExerciseMania", message:"Welcome to ExerciseMania!"})
+    res.render('index', { title: "ExerciseMania", message: "Welcome to ExerciseMania!" })
 })
 
 app.get('/login', (req, res) => {
     res.render('login', {})
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}`);
 })
