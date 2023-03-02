be796f3fa0df923cb38ea9198e4ce1fb69e2157a
Manuel Reyes <drakanez@gmail.com>
2023-03-02 14:20:22 -0500

    Cleanup

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-//import './assets/main.css'
-import 'bulma/css/bulma.min.css'
-
 const app = createApp(App)
 
 app.use(router)
