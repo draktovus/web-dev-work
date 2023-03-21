e2b9f113a974404d16bc011993b590916c7a7048
Manuel Reyes <drakanez@gmail.com>
2023-03-21 15:08:21 -0400

    Removed Cors from server

--- server/index.js
+++ server/index.js
@@ -2,26 +2,16 @@
 const http = require('http')
 const express = require('express');
 const app = express();
-const cors = require('cors')
 
 // 127.0.0.1 is the loopback address
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'pug');
-
-// Allows client VueJS to access server API
-app.use(cors({
-    origin: 'http://localhost:5173'
-}))
-
-app.get('/', (req, res) => {
-    res.send({ title: "ExerciseMania", message: "Welcome to ExerciseMania!" })
-})
-
-app.get('/login', (req, res) => {
-    res.render('login', {})
-})
+// Actions
+app
+    .get('/', (req, res) => {
+        res.send("Exercise Mania")
+    })
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}`);
