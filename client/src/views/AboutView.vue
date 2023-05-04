509684ee8b0c5f8b278304591e51c7236817ac68
Draktovus <drakanez@gmail.com>
2023-05-03 22:50:04 -0400

    updated about

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -2,13 +2,11 @@
   <div class="section">
     <div class="columns">
       <div class="column">
-        <h1>This is an about page</h1>
-      </div>
-      <div class="column">
-        <p class="bd-notification is-primary">
-          <code class="html">is-narrow</code><br />
-          First Column
-        </p>
+        <h1 class="title">Hello, if you got here than you did something you weren't suppose to! </h1>
+        <p class="content">That's okay. This web app is about exercise and tracking your progress. 
+          I don't know what to put here, and I think there should be an about page too so I have not deleted it yet.
+          Once logged in, users are able to enter in workouts and this app will be able to calculate a couple of things for you, even swapping between two measurement systems! 
+          How neat.</p>
       </div>
     </div>
   </div>
